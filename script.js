d184281f81b9b291cff417ad13df98060c03cc8b
Ihnatsy <ihpo5550@gmail.com>
2024-05-05 21:17:42 +0300

    add validation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*-----------burger menu----------------------*/
+
 const menu = document.getElementById('menu')
 const burger = document.getElementById('burger')
 const closingCross = document.getElementById('close-menu')
@@ -44,3 +46,98 @@ anchors.forEach(anchor => {
         })
     })
 })
+
+/*-----------validation----------------------*/
+
+
+function validation(form) {
+    let result = true
+
+    let inputsArr = []
+    let inputs = form.getElementsByClassName('mess')
+    for (let i = 0; i < inputs.length; i++) {
+        inputsArr.push(inputs[i])
+    }
+
+    inputsArr.forEach(input => {
+        removeError(input)
+        if (input.value === '') {
+            createError(input, 'Поле не заполнено')
+            result = false
+        }
+    })
+
+    const checkBox = document.getElementById('checkbox')
+    removeCheckboxError(checkBox)
+    if (checkBox.checked === false) {
+        readPrivacyPolicy(checkBox, 'Ознакомтесь с политикой конфиденциальности!')
+        result = false
+    }
+    return result
+}
+
+const messageForm = document.getElementById('message-form')
+messageForm.addEventListener('submit', sendMeMessage)
+
+function sendMeMessage(event) {
+    event.preventDefault()
+
+    if (validation(this) === true) {
+        alert('Сообщение отправлено')
+        clearInput()
+    }
+}
+
+function createError(input, text) {
+    const parent = input.parentNode //fieldset
+    parent.classList.add('error') //fieldset
+    const grandparent = parent.parentNode
+
+    const errorText = document.createElement('span')
+    errorText.classList.add('error-span')
+    errorText.append(text)
+    grandparent.append(errorText)
+}
+
+function removeError(input) {
+    const parent = input.parentNode //fieldset
+    const grandparent = parent.parentNode
+
+    if (parent.classList.contains('error')) {
+        grandparent.querySelector('span').remove()
+        parent.classList.remove('error')
+    }
+}
+
+function readPrivacyPolicy(input, text) {
+    const parent = input.parentNode
+    parent.classList.add('error-checkbox')
+
+    const errorText = document.createElement('span')
+    errorText.classList.add('error-span')
+    errorText.append(text)
+
+    parent.append(errorText)
+}
+
+function removeCheckboxError(input) {
+    const parent = input.parentNode
+    if (parent.classList.contains('error-checkbox')) {
+        parent.querySelector('.error-span').remove()
+        parent.classList.remove('error-checkbox')
+    }
+}
+
+function clearInput() {
+    const inputArr = []
+
+    let allInputs = document.getElementsByClassName('mess')
+    for (let i = 0; i < allInputs.length; i++) {
+        inputArr.push(allInputs[i])
+        inputArr[i].value = ''
+    }
+
+    const checkBox = document.getElementById('checkbox')
+    checkBox.checked = false
+}
+
